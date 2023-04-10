003b2724bc17c29663727a6e505118d30f706c09
shellcodesniper <shellcodesniper@icloud.com>
2023-04-10 15:09:50 +0900

    Push

--- src/qlgl/index.rs
+++ src/qlgl/index.rs
@@ -1,11 +1,10 @@
 use std::fmt::Debug;
 
-pub trait IndexTrait: Debug + Send + Sync {
+pub trait IndexTrait: Debug + Send + Sync + PartialOrd + PartialEq {
 
   fn to_bytes(&self) -> Vec<u8>;         // 인덱스를 바이트로 변환
   fn get_size() -> u64;             // 인덱스의 크기를 반환
 }
-
 
 impl IndexTrait for i64 {
   fn get_size() -> u64 {
@@ -14,7 +13,31 @@ impl IndexTrait for i64 {
 
   fn to_bytes(&self) -> Vec<u8> {
     let mut bytes = Vec::new();
-    bytes.extend_from_slice(&self.to_le_bytes());
+    bytes.extend_from_slice(&self.to_be_bytes());
+    bytes
+  }
+}
+
+// impl PartialEq for i64 {
+//   fn eq(&self, other: &Self) -> bool {
+//     self.cmp(other) == std::cmp::Ordering::Equal
+//   }
+// }
+//
+// impl PartialOrd for i64 {
+//   fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+//     Some(self.cmp(other))
+//   }
+// }
+
+impl IndexTrait for u64 {
+  fn get_size() -> u64 {
+    8
+  }
+
+  fn to_bytes(&self) -> Vec<u8> {
+    let mut bytes = Vec::new();
+    bytes.extend_from_slice(&self.to_be_bytes());
     bytes
   }
 }
--- src/qlgl/tree/nodes.rs
+++ src/qlgl/tree/nodes.rs
@@ -1,84 +1,80 @@
+use std::cmp;
+use std::fs::File;
+use std::io::{SeekFrom, Seek};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
 use super::{ IndexTrait, ValueTrait };
 
-const DEGREE: usize = 3;
-
 pub struct Node<I: IndexTrait, V: ValueTrait> {
+  pub is_root: bool,            // TYPE : 노드가 루트 노드인지 여부
+  pub is_leaf: bool,            // TYPE : 노드가 리프 노드인지 여부
+  pub is_dirty: bool,           // TYPE : 노드가 변경되었는지 여부
 
-  pub keys: Vec<I>,
-  pub values: Vec<V>,
-  pub children: Vec<Node<I, V>>,
+  offset: u64,                  // TYPE : GL(Value File) 내부 오프셋
 
-  pub is_leaf: bool,
-  pub is_root: bool,
+  pub keys: Vec<I>,             // TYPE : 키 벡터
+  pub values: Vec<V>,           // TYPE : 값 벡터
+  pub children: Vec<u64>,       // TYPE : 자식 노드 벡터
 
 
   pub phantom: std::marker::PhantomData<(I, V)>,
 }
 
 
-
-impl<I: IndexTrait, V: ValueTrait> Default for Node<I, V> {
-  fn default() -> Self {
-    Self::new()
-  }
-}
-
-
 // === 본 구현체 ===
 
 impl<I: IndexTrait, V: ValueTrait> Node<I, V> {
-  pub fn new() -> Self {
+  pub fn new(degree: usize) -> Self {
     Self {
-      keys: Vec::new(),
-      values: Vec::new(),
-      children: Vec::new(),
-      is_leaf: true,
       is_root: false,
+      is_leaf: false,
+      is_dirty: false,
+
+      offset: 0,
+
+      keys: Vec::with_capacity(degree - 1),
+      values: Vec::with_capacity(degree - 1),
+      children: Vec::with_capacity(degree),
+
       phantom: std::marker::PhantomData,
     }
   }
 
-  pub fn is_full(&self) -> bool {
-    self.keys.len() == DEGREE - 1
+  pub fn set_root(&mut self) {
+    self.is_root = true;
   }
 
-  pub fn is_empty(&self) -> bool {
-    self.keys.is_empty()
+  pub fn set_leaf(&mut self) {
+    self.is_leaf = true;
   }
 
-  pub fn is_underflow(&self) -> bool {
-    self.keys.len() < (DEGREE - 1) / 2
+  pub fn set_dirty(&mut self) {
+    self.is_dirty = true;
   }
 
-  pub fn is_overflow(&self) -> bool {
-    self.keys.len() > DEGREE - 1
+}
+
+impl<I: IndexTrait, V:ValueTrait> Node<I, V> {
+  pub fn write(&self, node_file: &mut File) -> Result<u64> {
+    // let mut buf = Vec::new();
+    let offset = node_file.seek(SeekFrom::End(0))?;
+    Ok(offset)
   }
 
-  pub fn is_root(&self) -> bool {
-    self.is_root
+  pub fn read(&mut self, node_file: &mut File, value_file: &mut File, offset: u64) -> Result<()> {
+    todo!("read")
+  }
+}
+
+
+impl<I: IndexTrait, V:ValueTrait> Node<I, V> {
+  pub fn insert(&mut self, key: I, value: V) -> Result<()> {
+    todo!("insert")
   }
 
-  pub fn is_leaf(&self) -> bool {
-    self.is_leaf
+  pub fn update(&mut self, key: I, value: V) -> Result<()> {
+    todo!("upate")
   }
-
-  pub fn is_internal(&self) -> bool {
-    !self.is_leaf
-  }
-
-  pub fn is_valid(&self) -> bool {
-    self.keys.len() == self.values.len()
-  }
-
-  pub fn append(&mut self, key: I, value: V) {
-    self.keys.push(key);
-    self.values.push(value);
-  }
-
-  pub fn append_child(&mut self, child: Node<I, V>) {
-    self.children.push(child);
-  }
-
-
 
 }
